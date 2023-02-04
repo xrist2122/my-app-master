ab174f38914dee72922469eac7182b6a2d864152
jopa2288 <jakkda@com>
2023-02-04 13:21:53 +0300

    g

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { useState } from 'react';
 
 function App() {
   const[vopros,setText]=useState(false)
+  let daa={
+    color:"green"
+  };
+  let nee={
+    color:"crimson"
+  };
 
 
 
   return <>
-  <button Onclick={() => setText(!vopros)}>btn1</button>
-  <p>{vopros ?"есть ":"нет"}</p>
+  <button onClick={() => setText(!vopros)}>btn1</button>
+  <p>{vopros ?<h3 style={daa}>еестьь</h3>:<h3 style={nee}>нееет</h3>}</p>
   </>
     
   ;
