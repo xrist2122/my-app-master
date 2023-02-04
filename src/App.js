e514c0bab72e1d4e3f57afc41227c4d9c8630ce4
jopa2288 <jakkda@com>
2023-02-04 13:58:45 +0300

    g

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const[vopros,setText]=useState(false)
-  let daa={
-    color:"green"
-  };
-  let nee={
-    color:"crimson"
-  };
+  // const[vopros,setText]=useState(false)
+  // let daa={
+  //   color:"green"
+  // };
+  // let nee={
+  //   color:"crimson"
+  // };
+  const[smena,setChange]=useState(true)
+  let jopa;
+  if(smena){
+    kvadat.style.backgroundColor="white"
+    krud.style.backgroundColor="white";
+
+  }else{
+    kvadat.style.backgroundColor="black"
+    krud.style.backgroundColor="black";
+
+  }
 
 
 
-  return <>
-  <button onClick={() => setText(!vopros)}>btn1</button>
-  <p>{vopros ?<h3 style={daa}>еестьь</h3>:<h3 style={nee}>нееет</h3>}</p>
-  </>
+  return <div className='krud'>
+  <input type="checkbox" checked={smena} onChange={() => setChange(!smena)} />
+  <button className='kvadat'>fjbg</button>
+  
+  {/* <button onClick={() => setText(!vopros)}>btn1</button>
+  <p>{vopros ?<h3 style={daa}>еестьь</h3>:<h3 style={nee}>нееет</h3>}</p> */}
+  </div>
     
   ;
 
